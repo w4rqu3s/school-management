662cb5f402bba576154a10cde40efb44e29e6f36
Victor Marques <w4rqu3s@gmail.com>
2025-11-17 00:26:51 -0300

    Adição de mais exemplos nas seeders

--- database/seeders/AlunoSeeder.php
+++ database/seeders/AlunoSeeder.php
@@ -12,9 +12,14 @@ class AlunoSeeder extends Seeder
     {
         $foto_v = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.petlove.com.br%2Fhusky-siberiano%2Fr&psig=AOvVaw0ZECqixcZ-GvusGQNoRZ1B&ust=1763426188663000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCNjt49v495ADFQAAAAAdAAAAABAE";
         $foto_l = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpremierpet.com.br%2Ftutor%2Fpug-tudo-o-que-voce-precisa-saber%2F&psig=AOvVaw3tGjcnYOUR29oaV_yfQBJj&ust=1763426261184000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCJCxvfH495ADFQAAAAAdAAAAABAM";
+        $foto_g = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.polipet.com.br%2Fracas%2Frough-collie%3Fsrsltid%3DAfmBOooUiRiGHV8OK7GfXytZTnc3VBXfWmOWRT7Ps3zm-R02rhdTPxsr&psig=AOvVaw0ZPXnLPsz94Wba3Z4CwtHw&ust=1763435564657000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCJiO48mb-JADFQAAAAAdAAAAABAE";
+        $foto_a = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.petlove.com.br%2Fbuldogue-ingles%2Fr&psig=AOvVaw0OSN_nEi27PdYJbhQd4Oli&ust=1763435595790000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPj0vNWb-JADFQAAAAAdAAAAABAE";
+
         $data = [
             ["nome" => "VICTOR PECINE MARQUES", "ano" => 3, "foto" => "$foto_v",  "curso_id" => 1],
-            ["nome" => "LUCAS PECINE MARQUES", "ano" => 4, "foto" => $foto_l, "curso_id" => 2]
+            ["nome" => "LUCAS PECINE MARQUES", "ano" => 1, "foto" => $foto_l, "curso_id" => 2],
+            ["nome" => "GRACIELLE GASPAR PECINE", "ano" => 1, "foto" => $foto_g, "curso_id" => 6],
+            ["nome" => "ASAFE ELIAS MARQUES", "ano" => 3, "foto" => $foto_a, "curso_id" => 1],
         ];
         DB::table('alunos')->insert($data);
     }
--- database/seeders/CursoSeeder.php
+++ database/seeders/CursoSeeder.php
@@ -14,6 +14,10 @@ class CursoSeeder extends Seeder
             ["nome" => "TÉCNICO EM INFORMÁTICA", "duracao" => 4],
             ["nome" => "TÉCNICO EM MEIO AMBIENTE", "duracao" => 4],
             ["nome" => "TÉCNICO EM MECÂNICA", "duracao" => 4],
+            ["nome" => "TÉCNICO EM PRODUÇÃO CULTURAL", "duracao" => 4],
+            ["nome" => "TÉCNÓLOGO EM ANÁLISE E DESENVOLVIMENTO DE SISTEMAS", "duracao" => 3],
+            ["nome" => "TÉCNÓLOGO EM GESTÃO AMBIENTAL", "duracao" => 3],
+            ["nome" => "TÉCNÓLOGO EM MANUTENÇÃO INDUSTRIAL", "duracao" => 3],
         ];
         DB::table('cursos')->insert($data);
     }
