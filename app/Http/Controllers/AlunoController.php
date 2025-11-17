dedf8ce0351741e52dc35091377412658a546983
Victor Marques <w4rqu3s@gmail.com>
2025-11-16 22:40:52 -0300

    implementação inicial do controller

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -4,61 +4,79 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use \App\Models\Aluno;
+
 class AlunoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $alunos = Aluno::all();
+        return view('alunos.index', compact('alunos'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('alunos.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $aluno = new Aluno();
+
+        $aluno->nome = $request->nome;
+        $aluno->ano = $request->ano;
+        $aluno->curso = 1;
+
+        $aluno->save();
+
+        return redirect()->route('alunos.show', compact('aluno'));
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $aluno = Aluno::find($id);
+
+        if(isset($aluno)) {
+            return view('alunos.show', compact('aluno'));
+        }
+
+        return redirect()->route('alunos.index');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        //
+        $aluno = Aluno::find($id);
+
+        if(isset($aluno)) {
+            return view('alunos.edit', compact('aluno'));
+        }
+
+        return redirect()->route('alunos.index');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $aluno = Aluno::find($id);
+
+        if(isset($aluno)) {
+            $aluno->nome = $request->nome;
+            $aluno->ano = $request->ano;
+            $aluno->curso = $request->curso;
+
+            return redirect()->route('alunos.show', compact('aluno'));
+        }
+
+        return redirect()->route('alunos.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $aluno = Aluno::find($id);
+
+        if(isset($aluno)) {
+            $aluno->delete();
+        }
+
+        return redirect()->route('alunos.index');
     }
 }
