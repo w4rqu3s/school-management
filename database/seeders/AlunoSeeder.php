272a49bfa63cbc3e3781cf17f466ec537d02b4d4
Victor Marques <w4rqu3s@gmail.com>
2025-11-17 19:58:47 -0300

    Atualização do seeder de alunos

--- database/seeders/AlunoSeeder.php
+++ database/seeders/AlunoSeeder.php
@@ -2,25 +2,45 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class AlunoSeeder extends Seeder
 {
     public function run(): void
     {
-        $foto_v = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.petlove.com.br%2Fhusky-siberiano%2Fr&psig=AOvVaw0ZECqixcZ-GvusGQNoRZ1B&ust=1763426188663000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCNjt49v495ADFQAAAAAdAAAAABAE";
-        $foto_l = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpremierpet.com.br%2Ftutor%2Fpug-tudo-o-que-voce-precisa-saber%2F&psig=AOvVaw3tGjcnYOUR29oaV_yfQBJj&ust=1763426261184000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCJCxvfH495ADFQAAAAAdAAAAABAM";
-        $foto_g = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.polipet.com.br%2Fracas%2Frough-collie%3Fsrsltid%3DAfmBOooUiRiGHV8OK7GfXytZTnc3VBXfWmOWRT7Ps3zm-R02rhdTPxsr&psig=AOvVaw0ZPXnLPsz94Wba3Z4CwtHw&ust=1763435564657000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCJiO48mb-JADFQAAAAAdAAAAABAE";
-        $foto_a = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.petlove.com.br%2Fbuldogue-ingles%2Fr&psig=AOvVaw0OSN_nEi27PdYJbhQd4Oli&ust=1763435595790000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPj0vNWb-JADFQAAAAAdAAAAABAE";
+        // Certifique-se que a pasta 'fotos' existe
+        Storage::disk('public')->makeDirectory('fotos');
 
-        $data = [
-            ["nome" => "VICTOR PECINE MARQUES", "ano" => 3, "foto" => "$foto_v",  "curso_id" => 1],
-            ["nome" => "LUCAS PECINE MARQUES", "ano" => 1, "foto" => $foto_l, "curso_id" => 2],
-            ["nome" => "GRACIELLE GASPAR PECINE", "ano" => 1, "foto" => $foto_g, "curso_id" => 6],
-            ["nome" => "ASAFE ELIAS MARQUES", "ano" => 3, "foto" => $foto_a, "curso_id" => 1],
+        // Caminhos dos arquivos de teste
+        $fotos_testes = [
+            database_path('seeders/fotos_testes/victor_marques__husky.webp'),
+            database_path('seeders/fotos_testes/lucas_pecine__pug.jpg'),
+            database_path('seeders/fotos_testes/gracielle_gaspar__lassie.jpg'),
+            database_path('seeders/fotos_testes/catnap.webp'),
         ];
+
+        // Dados iniciais sem foto
+        $data = [
+            ["nome" => "VICTOR PECINE MARQUES", "ano" => 3, "curso_id" => 1],
+            ["nome" => "LUCAS PECINE MARQUES", "ano" => 1, "curso_id" => 2],
+            ["nome" => "GRACIELLE GASPAR PECINE", "ano" => 1, "curso_id" => 6],
+            ["nome" => "CATNAP", "ano" => 3, "curso_id" => 1],
+        ];
+
+        foreach ($data as &$aluno) {
+            $foto_origem = array_shift($fotos_testes); // pega fotos na ordem
+
+            $ext = pathinfo($foto_origem, PATHINFO_EXTENSION);
+            $nome_arquivo = Str::uuid() . '.' . $ext;
+
+            Storage::disk('public')->putFileAs('fotos', $foto_origem, $nome_arquivo);
+
+            $aluno['foto'] = 'fotos/'.$nome_arquivo;
+        }
+
         DB::table('alunos')->insert($data);
     }
 }
