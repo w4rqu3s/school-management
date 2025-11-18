f2bd69731887a700839581536260fca3651d4d9d
Marques <145288183+w4rqu3s@users.noreply.github.com>
2025-11-17 23:01:58 -0300

    Update AlunoSeeder.php

--- database/seeders/AlunoSeeder.php
+++ database/seeders/AlunoSeeder.php
@@ -16,25 +16,10 @@ class AlunoSeeder extends Seeder
 
         // Caminhos dos arquivos de teste
         $fotos_testes = [
-            database_path('seeders/fotos_testes/husky.webp'),
-            database_path('seeders/fotos_testes/lucaspug.jpg'),
-            database_path('seeders/fotos_testes/lassie.jpg'),
-            database_path('seeders/fotos_testes/catnap.webp'),
-            database_path('seeders/fotos_testes/asafepug7.webp'),
-            database_path('seeders/fotos_testes/lulu.webp'),
-            database_path('seeders/fotos_testes/chihuahua.avif'),
-            database_path('seeders/fotos_testes/beagle.webp'),
         ];
 
         // Dados iniciais sem foto
         $data = [
-            ["nome" => "VICTOR PECINE MARQUES", "ano" => 3, "curso_id" => 1],
-            ["nome" => "LUCAS PECINE MARQUES", "ano" => 1, "curso_id" => 2],
-            ["nome" => "GRACIELLE GASPAR PECINE", "ano" => 1, "curso_id" => 6],
-            ["nome" => "CATNAP", "ano" => 3, "curso_id" => 1],
-            ["nome" => "ASAFE ELIAS MARQUES", "ano" => 3, "curso_id" => 1],
-            ["nome" => "HANAE ROSA TERATO RAMOS", "ano" => 3, "curso_id" => 1],
-            ["nome" => "DANIEL GARBETTO CARDOSO", "ano" => 3, "curso_id" => 1],
         ];
 
         foreach ($data as &$aluno) {
