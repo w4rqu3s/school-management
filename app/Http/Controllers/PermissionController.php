b6fafd126706e2e1791ec50ac0d2f4f505f5b162
Victor Marques <w4rqu3s@gmail.com>
2025-11-18 20:18:31 -0300

    implementação de PermissionController

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use \App\Models\Permission;
+
+class PermissionController extends Controller
+{
+    public static function loadPermission($role) {
+
+        $array_permissions = Array();
+        $perm = Permission::with(['resource'])  
+            ->where('role_id', $role)->get();   // carrega todas as permissões daquele role, já com os seus resources
+        foreach($perm as $item) {
+            $array_permissions[$item->resource->name] =
+                (boolean) $item->permission;                // vê se tem ou não a permissão e atribui
+        }
+        // dd($arr_permissions)
+        session(['user_permissions' => $arr_permissions]);
+    }
+
+    public static function isAuthorized($resource) {
+        $permissions = session('user_permissions');
+
+        if(array_key_exists($resource, $permissions)) {
+            return $permissions([$resource]);
+        }
+
+        return false;
+    }
+}
--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -12,45 +12,45 @@ class PermissionSeeder extends Seeder
     {
         $data = [
             // PROFESSOR - CURSO
-            ["role_id" => 1, "resource_id" => 1, "permission" => 0],
-            ["role_id" => 1, "resource_id" => 2, "permission" => 0],
-            ["role_id" => 1, "resource_id" => 3, "permission" => 0],
-            ["role_id" => 1, "resource_id" => 4, "permission" => 0],
-            ["role_id" => 1, "resource_id" => 5, "permission" => 0],
-            ["role_id" => 1, "resource_id" => 6, "permission" => 0],
-            ["role_id" => 1, "resource_id" => 7, "permission" => 0],
+            ["role_id" => 1, "resource_id" => 1, "permission" => 0],    // ver cursos
+            ["role_id" => 1, "resource_id" => 2, "permission" => 0],    // criar curso
+            ["role_id" => 1, "resource_id" => 3, "permission" => 0],    // ver um curso
+            ["role_id" => 1, "resource_id" => 4, "permission" => 0],    // editar curso
+            ["role_id" => 1, "resource_id" => 5, "permission" => 0],    // deletar curso
+            ["role_id" => 1, "resource_id" => 6, "permission" => 0],    // ver alunos do curso
+            ["role_id" => 1, "resource_id" => 7, "permission" => 0],    // ver disciplinas do curso
             // PROFESSOR - ALUNO
-            ["role_id" => 1, "resource_id" => 8, "permission" => 1],
-            ["role_id" => 1, "resource_id" => 9, "permission" => 1],
-            ["role_id" => 1, "resource_id" => 10, "permission" => 1],
-            ["role_id" => 1, "resource_id" => 11, "permission" => 1],
-            ["role_id" => 1, "resource_id" => 12, "permission" => 1],
-            ["role_id" => 1, "resource_id" => 13, "permission" => 1],
+            ["role_id" => 1, "resource_id" => 8, "permission" => 1],    // ver alunos
+            ["role_id" => 1, "resource_id" => 9, "permission" => 1],    // criar aluno
+            ["role_id" => 1, "resource_id" => 10, "permission" => 1],   // ver um aluno
+            ["role_id" => 1, "resource_id" => 11, "permission" => 1],   // editar aluno
+            ["role_id" => 1, "resource_id" => 12, "permission" => 0],   // deletar aluno
+            ["role_id" => 1, "resource_id" => 13, "permission" => 1],   // gerar pdf de aluno
             // PROFESSOR - DISCIPLINA
-            ["role_id" => 1, "resource_id" => 14, "permission" => 2],
-            ["role_id" => 1, "resource_id" => 15, "permission" => 2],
-            ["role_id" => 1, "resource_id" => 16, "permission" => 2],
-            ["role_id" => 1, "resource_id" => 17, "permission" => 2],
+            ["role_id" => 1, "resource_id" => 14, "permission" => 0],   // ver disciplinas
+            ["role_id" => 1, "resource_id" => 15, "permission" => 0],   // criar disciplina
+            ["role_id" => 1, "resource_id" => 16, "permission" => 0],   // ver uma disciplina
+            ["role_id" => 1, "resource_id" => 17, "permission" => 0],   // deletar disciplina
             // COORDERNADOR - CURSO
-            ["role_id" => 2, "resource_id" => 1, "permission" => 0],
-            ["role_id" => 2, "resource_id" => 2, "permission" => 0],
-            ["role_id" => 2, "resource_id" => 3, "permission" => 0],
-            ["role_id" => 2, "resource_id" => 4, "permission" => 0],
-            ["role_id" => 2, "resource_id" => 5, "permission" => 0],
-            ["role_id" => 2, "resource_id" => 6, "permission" => 0],
-            ["role_id" => 2, "resource_id" => 7, "permission" => 0],
+            ["role_id" => 2, "resource_id" => 1, "permission" => 1],    // ver cursos
+            ["role_id" => 2, "resource_id" => 2, "permission" => 1],    // criar curso
+            ["role_id" => 2, "resource_id" => 3, "permission" => 1],    // ver um curso
+            ["role_id" => 2, "resource_id" => 4, "permission" => 1],    // editar curso
+            ["role_id" => 2, "resource_id" => 5, "permission" => 1],    // deletar curso
+            ["role_id" => 2, "resource_id" => 6, "permission" => 1],    // ver alunos do curso
+            ["role_id" => 2, "resource_id" => 7, "permission" => 1],    // ver disciplinas do curso
             // COORDENADOR - ALUNO
-            ["role_id" => 2, "resource_id" => 8, "permission" => 1],
-            ["role_id" => 2, "resource_id" => 9, "permission" => 1],
-            ["role_id" => 2, "resource_id" => 10, "permission" => 1],
-            ["role_id" => 2, "resource_id" => 11, "permission" => 1],
-            ["role_id" => 2, "resource_id" => 12, "permission" => 1],
-            ["role_id" => 2, "resource_id" => 13, "permission" => 1],
+            ["role_id" => 2, "resource_id" => 8, "permission" => 1],    // ver alunos
+            ["role_id" => 2, "resource_id" => 9, "permission" => 1],    // criar aluno
+            ["role_id" => 2, "resource_id" => 10, "permission" => 1],   // ver um aluno
+            ["role_id" => 2, "resource_id" => 11, "permission" => 1],   // editar aluno
+            ["role_id" => 2, "resource_id" => 12, "permission" => 1],   // deletar aluno
+            ["role_id" => 2, "resource_id" => 13, "permission" => 1],   // gerar pdf de aluno
             // COORDENADOR - DISCIPLINA
-            ["role_id" => 2, "resource_id" => 14, "permission" => 2],
-            ["role_id" => 2, "resource_id" => 15, "permission" => 2],
-            ["role_id" => 2, "resource_id" => 16, "permission" => 2],
-            ["role_id" => 2, "resource_id" => 17, "permission" => 2],
+            ["role_id" => 2, "resource_id" => 14, "permission" => 1],   // ver disciplinas
+            ["role_id" => 2, "resource_id" => 15, "permission" => 1],   // criar disciplina
+            ["role_id" => 2, "resource_id" => 16, "permission" => 1],   // ver uma disciplina
+            ["role_id" => 2, "resource_id" => 17, "permission" => 1],   // deletar disciplina
         ];
         DB::table('permissions')->insert($data);
     }
